d60ad2b153c8a63b119a45c05aaf50b6097d2951
hnprivv <huzaifanajmuddin707@gmail.com>
2026-07-30 22:30:26 +0500

    add about endpoint

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"message": "Hello Server!"}
+
+@app.get("/about")
+def desc():
+    return {"name": "Task API", "version": "1.0", "endpoints": ["/tasks"]}
